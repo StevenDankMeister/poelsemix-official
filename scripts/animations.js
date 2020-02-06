9ad6bc8d0e55125580b5909e7132346a66756fc7
Steven <thedankpure@gmail.com>
2020-02-06 20:13:54 +0100

    epic animation

---
+++ scripts/animations.js
@@ -0,0 +1,15 @@
+const fadeInOnScroll = (scroll_height, container) =>{
+  let opacity = 0;
+  let running = false;
+  $(window).on("scroll", () =>{
+    if($(window).scrollTop() > scroll_height && !running){
+      running = true;
+      setInterval(function(){
+        if(opacity < 1){
+          opacity += 0.01;
+          $(container).css("opacity", opacity);
+        }
+      }, 10);
+    }
+  });    
+}
